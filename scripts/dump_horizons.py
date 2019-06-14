ab13d9366fe0cb99273e60c4f87dcf141125c1b6
Alexander Koryagin <a.koryagin@analysiscenter.org>
2019-06-14 16:40:46 +0300

    update dnp-horizons script; add json-config

--- scripts/dump_horizons.py
+++ scripts/dump_horizons.py
@@ -1,7 +1,8 @@
-"""Test"""
+""" Predict horizons-mask using saved model and dump largest horizons on disk. """
 #pylint: disable=import-error, wrong-import-position
 import os
 import sys
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -11,29 +12,6 @@ sys.path.append('..')
 from seismiqb import SeismicCubeset
 from seismiqb.batchflow import Pipeline, FilesIndex, B, V, L, D
 from seismiqb.batchflow.models.tf import TFModel
-
-os.environ['CUDA_VISIBLE_DEVICES'] = '7'
-
-# Script-args
-# Cube crop for prediction
-CUBE_CROP = ([2000, 2500], [0, 1200], [500, 600])
-
-# Small-crop shape
-CROP_SHAPE = [3, 400, 400]
-
-# Stride of crops
-CROP_STRIDE = [3, 400, 400]
-
-# Threshold of covered area by horizon to be dumped
-AREA_SHARE = 0.6
-
-# Threshold for clearing predictions
-THRESHOLD = 0.5
-
-PATH_TO_MODEL = "/notebooks/SEISMIC_DATA/SAVED/MODELS/CUBE_3_ONLY/"
-PATH_TO_CUBE = "/notebooks/SEISMIC_DATA/CUBE_3/P_cube.hdf5"
-PATH_TO_PREDICTIONS = "/notebooks/SEISMIC_DATA/CUBE_3/SCRIPT/"
-
 
 def dump_horizon(horizon, geometry, path, name):
     """ Convert horizon to point cloud and save it to desired place. """
@@ -48,22 +26,26 @@ def dump_horizon(horizon, geometry, path, name):
     labels.to_csv(os.path.join(path, name + '.csv'), sep=' ', index=False, header=False)
 
 
-def main():
+def main(path_to_cube, path_to_model, path_to_predictions, gpu_devices,
+         cube_crop, crop_shape, crop_stride, area_share, threshold):
     """ Main function. """
+    # set gpu-devices
+    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_devices
+
     # Init Cubeset and load cube-geometries
-    dsix = FilesIndex(path=PATH_TO_CUBE, no_ext=True)
+    dsix = FilesIndex(path=path_to_cube, no_ext=True)
     ds = SeismicCubeset(dsix)
     ds = ds.load_geometries()
 
     # Make grid for small crops
-    ds = ds.make_grid(ds.indices[0], CROP_SHAPE, *CUBE_CROP, CROP_STRIDE)
+    ds = ds.make_grid(ds.indices[0], crop_shape, *cube_crop, crop_stride)
 
     # Pipeline: slice crops, normalize values in the cube, make predictions
     # via model, assemble crops according to the grid
-    load_config = {'load/path': PATH_TO_MODEL}
+    load_config = {'load/path': path_to_model}
     predict_pipeline = (Pipeline()
                         .load_component(src=D('geometries'), dst='geometries')
-                        .crop(points=L(D('grid_gen')), shape=CROP_SHAPE)
+                        .crop(points=L(D('grid_gen')), shape=crop_shape)
                         .load_cubes(dst='data_crops')
                         .rotate_axes(src='data_crops')
                         .scale(mode='normalize', src='data_crops')
@@ -80,20 +62,29 @@ def main():
 
     # Fetch and dump horizons
     prediction = batch.assembled_pred
-    ds.get_point_cloud(prediction, 'horizons', coordinates='lines', threshold=THRESHOLD)
+    ds.get_point_cloud(prediction, 'horizons', coordinates='lines', threshold=threshold)
 
-    if (not os.path.exists(PATH_TO_PREDICTIONS)) and \
-       (not os.path.isdir(PATH_TO_PREDICTIONS)):
-        os.mkdir(PATH_TO_PREDICTIONS)
+    if (not os.path.exists(path_to_predictions)) and \
+       (not os.path.isdir(path_to_predictions)):
+        os.mkdir(path_to_predictions)
 
     ctr = 0
     for h in ds.horizons:
-        if len(h) / np.prod(ds.geometries[ds.indices[0]].cube_shape) >= AREA_SHARE:
+        if len(h) / np.prod(ds.geometries[ds.indices[0]].cube_shape) >= area_share:
             dump_horizon(h, ds.geometries[ds.indices[0]],
-                         PATH_TO_PREDICTIONS, 'horizon_' + str(ctr))
+                         path_to_predictions, 'horizon_' + str(ctr))
             ctr += 1
 
 
-
 if __name__ == '__main__':
-    main()
+    # fetch path to config
+    parser = argparse.ArgumentParser(description="Predict horizons on a part of seismic-cube.")
+    parser.add_argument("--config_path", type=str, default="./config.json")
+    args = parser.parse_args()
+
+    # fetch main-arguments from config and run main
+    with open(args.config_path, 'r') as file:
+        config = json.load(file)
+        args = [config.get(key) for key in ["cubePath", "modelPath", "predictionsPath", "gpuDevices",
+                                            "cubeCrop", "cropShape", "cropStride", "areaShare", "threshold"]]
+        main(*args)
