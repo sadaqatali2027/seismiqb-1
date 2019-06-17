9c25fd3c2dad288dd2740bd754249555d6fbcb8e
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2019-06-17 08:15:34 +0000

    Add logging

--- scripts/compare_horizonts.py
+++ scripts/compare_horizonts.py
@@ -3,36 +3,36 @@
 import os
 import sys
 import argparse
+import json
+import logging
 
 import numpy as np
-import pandas as pd
 from tqdm import tqdm
 from glob import glob
 
 sys.path.append('..')
-from seismiqb import SeismicCubeset, read_point_cloud, make_labels_dict
-from seismiqb.batchflow import Pipeline, FilesIndex, B, V, L, D
-from seismiqb.batchflow.models.tf import TFModel
+from seismiqb import read_point_cloud, make_labels_dict
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '6'
-
-# Script-args
-LIST_1 = '/notebooks/SEISMIC_DATA/CUBE_3/LINE_HORIZONTS/prb_G_anon.txt'
-LIST_2 = '/notebooks/SEISMIC_DATA/CUBE_3/PREDICTED_HORIZONTS/Hor_10-2731_0-2550_500-600_EACH_100_SAME_CUBE_1+VAL_1.txt'
 
 
 def compare(horizont_1, horizont_2):
-    print('\nComparing: ')
-    print('           {}'.format(horizont_1))
-    print('           {}'.format(horizont_2))
-
+    """ Compare two horizonts by computing multiple simple metrics.
+    
+    Parameters
+    ----------
+    horizont_1, horizont_2 : str
+        Path to horizont. Each line in the file must be in (iline, xline, height) format.
+        
+    Returns
+    -------
+    dict
+        Computed metrics.
+    """
     point_cloud_1 = read_point_cloud(horizont_1)
     labels_1 = make_labels_dict(point_cloud_1)
-    print('First horizont is processed..')
 
     point_cloud_2 = read_point_cloud(horizont_2)
     labels_2 = make_labels_dict(point_cloud_2)
-    print('Second horizont is processed..')
 
     differences = []
     not_present_1, not_present_2 = 0, 0
@@ -53,8 +53,8 @@ def compare(horizont_1, horizont_2):
         if labels_1.get(key) is None:
             not_present_2 += 1
 
-    info = {'name_1': '/'.join(horizont_1.split('/')[-3:])
-            'name_2': '/'.join(horizont_2.split('/')[-3:])
+    info = {'name_1': '/'.join(horizont_1.split('/')[-3:]),
+            'name_2': '/'.join(horizont_2.split('/')[-3:]),
             'mean_error': np.mean(differences),
             'std_error':  np.std(differences),
             'len_1': len(labels_1),
@@ -66,42 +66,67 @@ def compare(horizont_1, horizont_2):
             'not_present_1': not_present_1,
             'not_present_2': not_present_2,
             }
-
     return info
 
 
-def main(list_1, list_2):
-    list_1 = glob(list_1)
-    list_2 = glob(list_2)
+def main(dir_1, dir_2, printer=None):
+    """ Compare each pair of horizonts in passed lists.
+    
+    Parameters
+    ----------
+    dir_1, dir_2 : str
+        Path to directories with horizonts to compare.
+        
+    printer : callable
+        Function to print with.
+    """
+    list_1 = glob(dir_1)
+    list_2 = glob(dir_2)
     cross = [(item_1, item_2) for item_1 in list_1 for item_2 in list_2]
 
     for horizont_1, horizont_2 in tqdm(cross):
         info = compare(horizont_1, horizont_2)
 
-        print('First horizont:  {}'.format(info['name_1']))
-        print('Second horizont: {}'.format(info['name_2']))
+        printer('First horizont:  {}'.format(info['name_1']))
+        printer('Second horizont: {}'.format(info['name_2']))
 
-        print('Mean value/std of error:                  {:8.7} / {:8.7}'.format(info['mean_error'], info['std_error']))
-        print('First horizont length:                    {}'.format(info['len_1']))
-        print('Second horizont length:                   {}'.format(info['len_2']))
+        printer('Mean value/std of error:                  {:8.7} / {:8.7}'.format(info['mean_error'], info['std_error']))
+        printer('First horizont length:                    {}'.format(info['len_1']))
+        printer('Second horizont length:                   {}'.format(info['len_2']))
 
-        print('Number in 5 ms window:                    {}'.format(info['in_window']))
-        print('Rate in 5 ms window:                      {:8.7}'.format(info['rate_in_window']))
+        printer('Number in 5 ms window:                    {}'.format(info['in_window']))
+        printer('Rate in 5 ms window:                      {:8.7}'.format(info['rate_in_window']))
 
-        print('Average height of FIRST horizont:         {:8.7}'.format(info['mean_1']))
-        print('Average height of SECOND horizont:        {:8.7}'.format(info['mean_2']))
+        printer('Average height of FIRST horizont:         {:8.7}'.format(info['mean_1']))
+        printer('Average height of SECOND horizont:        {:8.7}'.format(info['mean_2']))
 
-        print('In the FIRST, but not in the SECOND:      {}'.format(info['not_present_1']))
-        print('In the SECOND, but not in the FIRST:      {}'.format(info['not_present_2']))
-        print('\n\n')
+        printer('In the FIRST, but not in the SECOND:      {}'.format(info['not_present_1']))
+        printer('In the SECOND, but not in the FIRST:      {}'.format(info['not_present_2']))
+        printer('\n\n')
 
 
 if __name__ == '__main__':
+    # Get arguments from passed json
     parser = argparse.ArgumentParser(description="Compare two lists of horizonts.")
     parser.add_argument("--config_path", type=str, default="./config_compare.json")
     args = parser.parse_args()
 
     with open(args.config_path, 'r') as file:
         config = json.load(file)
-        args = [config.get(key) for key in ["list_1", "list_2"]]
-    main(*args)
+        args = [config.get(key) for key in ["dir_1", "dir_2"]]
+        
+    # Logging to either stdout or file
+    if config.get("print"):
+        printer = print
+    else:
+        path_log = config.get("path_log") or os.path.join(os.getcwd(), "compare.log")
+        handler = logging.FileHandler(path_log, mode='w')        
+        handler.setFormatter(logging.Formatter('%(message)s'))
+
+        logger = logging.getLogger('compare_logger')
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        printer = logger.info
+    
+    # Compare each pair of horizonts in two directories
+    main(*args, printer=printer)
--- scripts/dump_horizons.py
+++ scripts/dump_horizons.py
@@ -3,6 +3,8 @@
 import os
 import sys
 import argparse
+import json
+import logging
 
 import numpy as np
 import pandas as pd
@@ -12,6 +14,8 @@ sys.path.append('..')
 from seismiqb import SeismicCubeset
 from seismiqb.batchflow import Pipeline, FilesIndex, B, V, L, D
 from seismiqb.batchflow.models.tf import TFModel
+
+
 
 def dump_horizon(horizon, geometry, path, name):
     """ Convert horizon to point cloud and save it to desired place. """
@@ -26,16 +30,17 @@ def dump_horizon(horizon, geometry, path, name):
     labels.to_csv(os.path.join(path, name + '.csv'), sep=' ', index=False, header=False)
 
 
-def main(path_to_cube, path_to_model, path_to_predictions, gpu_devices,
-         cube_crop, crop_shape, crop_stride, area_share, threshold):
+def main(path_to_cube, path_to_model, path_to_predictions, gpu_device,
+         cube_crop, crop_shape, crop_stride, area_share, threshold, printer=None):
     """ Main function. """
-    # set gpu-devices
-    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_devices
+    # Set gpu-device
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_device)
 
     # Init Cubeset and load cube-geometries
     dsix = FilesIndex(path=path_to_cube, no_ext=True)
     ds = SeismicCubeset(dsix)
     ds = ds.load_geometries()
+    printer('Cube assembling is started')
 
     # Make grid for small crops
     ds = ds.make_grid(ds.indices[0], crop_shape, *cube_crop, crop_stride)
@@ -59,32 +64,53 @@ def main(path_to_cube, path_to_model, path_to_predictions, gpu_devices,
 
     for _ in tqdm(range(ds.grid_iters)):
         batch = predict_pipeline.next_batch(1, n_epochs=None)
+    printer('Cube is assembled')
 
     # Fetch and dump horizons
     prediction = batch.assembled_pred
     ds.get_point_cloud(prediction, 'horizons', coordinates='lines', threshold=threshold)
+    printer('Horizonts are labeled')
 
     if (not os.path.exists(path_to_predictions)) and \
        (not os.path.isdir(path_to_predictions)):
         os.mkdir(path_to_predictions)
 
+    ds.horizons.sort(key=len, reverse=True)
+    area = (cube_crop[0][1] - cube_crop[0][0]) * (cube_crop[1][1] - cube_crop[1][0])
+
     ctr = 0
-    for h in ds.horizons:
-        if len(h) / np.prod(ds.geometries[ds.indices[0]].cube_shape) >= area_share:
+    for h in ds.horizons[:10]:
+        if len(h) / area >= area_share:
             dump_horizon(h, ds.geometries[ds.indices[0]],
-                         path_to_predictions, 'horizon_' + str(ctr))
+                         path_to_predictions, 'Horizon_' + str(ctr))
+            printer('Horizont {} is saved'.format(ctr))
             ctr += 1
 
 
 if __name__ == '__main__':
-    # fetch path to config
+    # Fetch path to config
     parser = argparse.ArgumentParser(description="Predict horizons on a part of seismic-cube.")
     parser.add_argument("--config_path", type=str, default="./config_dump.json")
     args = parser.parse_args()
 
-    # fetch main-arguments from config and run main
+    # Fetch main-arguments from config
     with open(args.config_path, 'r') as file:
         config = json.load(file)
-        args = [config.get(key) for key in ["cubePath", "modelPath", "predictionsPath", "gpuDevices",
+        args = [config.get(key) for key in ["cubePath", "modelPath", "predictionsPath", "gpuDevice",
                                             "cubeCrop", "cropShape", "cropStride", "areaShare", "threshold"]]
-        main(*args)
+        
+    # Logging to either stdout or file
+    if config.get("print"):
+        printer = print
+    else:
+        path_log = config.get("path_log") or os.path.join(os.getcwd(), "dump.log")
+        print('LOGGING TO ', path_log)
+        handler = logging.FileHandler(path_log, mode='w')        
+        handler.setFormatter(logging.Formatter('%(asctime)s     %(message)s'))
+
+        logger = logging.getLogger('train_logger')
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        printer = logger.info
+        
+    main(*args, printer=printer)
