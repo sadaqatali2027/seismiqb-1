1b039b0a3054dd9f37f0d48e868df95027a3f22e
Darima Mylzenova <d.mylzenova@analysiscenter.org>
2019-07-17 15:08:24 +0000

    upd tutorial

--- models/utils.py
+++ models/utils.py
@@ -1,5 +1,6 @@
 """ Util functions for tutorials. """
 import numpy as np
+import tensorflow as tf
 
 def get_lines_range(batch, item):
     g = batch.get(batch.indices[item], 'geometries')
@@ -11,3 +12,20 @@ def get_lines_range(batch, item):
           g.xlines[np.max(batch.slices[item][1])])
     xr = (np.array(xr) - g.xlines[0]) / (g.xlines[-1] - g.xlines[0])
     return ir, xr
+
+
+def make_data_extension(batch, **kwargs):
+    data_x = []
+    for i, cube in enumerate(batch.data_crops):
+        cut_mask_ = batch.cut_mask_crops[i]
+        data_x.append(np.concatenate([cube, cut_mask_], axis=-1))
+
+    data_y = []
+    
+    for cube in batch.mask_crops:
+        data_y.append(cube)
+    return {"feed_dict": {'cubes': data_x,
+                          'masks': data_y}}
+
+def predictions(x):
+    return tf.expand_dims(x, axis=-1, name='expand')
