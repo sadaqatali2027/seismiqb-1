11b3f745d946885073c0ab05556be6ee793290dd
Darima Mylzenova <d.mylzenova@analysiscenter.org>
2019-07-18 08:45:49 +0000

    rm plot function definitions from  tutorial

--- models/utils.py
+++ models/utils.py
@@ -1,6 +1,7 @@
 """ Util functions for tutorials. """
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def get_lines_range(batch, item):
     g = batch.get(batch.indices[item], 'geometries')
@@ -29,3 +30,59 @@ def make_data_extension(batch, **kwargs):
 
 def predictions(x):
     return tf.expand_dims(x, axis=-1, name='expand')
+
+def show_input_data(btch):
+    cv = 0.2
+    i = np.random.choice(64)
+
+    _, axes = plt.subplots(nrows=1, ncols=3, figsize=(30, 15))
+    axes[0].imshow(btch.data_crops[i][:, :, 0].T)
+    axes[0].set_title('Image', fontsize=20)
+    axes[1].imshow(btch.data_crops[i][:, :, 0].T, cmap='Greens')
+    axes[1].imshow(btch.mask_crops[i][:, :, 0].T, cmap='Greens', alpha=0.3)
+    axes[1].set_title('Mask', fontsize=20)
+    axes[2].imshow(btch.data_crops[i][:, :, 0].T, cmap='Greens')
+    axes[2].imshow(btch.cut_mask_crops[i][:, :, 0].T, cmap='Greens', alpha=0.3)
+    axes[2].set_title('Thin out mask', fontsize=20)
+    plt.show()
+
+def show_extension_results(val_batch, val_pipeline, cubes_numbers):
+    for cube in cubes_numbers:
+        print(val_batch.indices[cube][:-10])
+        iline = 0
+
+        truth_img =     val_pipeline.get_variable('ext_result')[0][0][cube, :, :, iline].T
+        truth_labels =  val_pipeline.get_variable('ext_result')[0][1][cube, :, :, iline, 0].T
+        predicted_img = val_pipeline.get_variable('ext_result')[0][2][cube, :, :, iline, 0].T
+        cut_mask =      val_pipeline.get_variable('ext_result')[0][0][cube, :, :, iline + 2].T
+        predicted_simple = val_pipeline.get_variable('result')[0][2][cube, :, :, iline, 0].T
+
+        fig = plt.figure(figsize=(25, 10))
+        fig.add_subplot(1, 5, 1)
+        plt.imshow(truth_img, cmap='gray')
+        plt.title('Input tensor', fontsize=20)
+
+
+        fig.add_subplot(1, 5, 2)
+        plt.imshow(cut_mask, cmap="Blues")
+        plt.imshow(truth_img, cmap="gray", alpha=0.5)
+
+        plt.title('Input mask', fontsize=20)
+
+        fig.add_subplot(1, 5, 3)Extension model prediction
+        plt.imshow(truth_labels, cmap="Blues")
+        plt.imshow(truth_img, cmap="gray", alpha=0.5)
+        plt.title('True mask', fontsize=20)
+
+        fig.add_subplot(1, 5, 4)
+        plt.imshow(predicted_simple, cmap="Greens")
+        plt.title('Baseline model prediction', fontsize=20)
+        plt.show()
+
+        fig.add_subplot(1, 5, 5)
+        plt.imshow(predicted_img, cmap="Blues")
+        plt.imshow(truth_img, cmap="gray", alpha=0.5)
+        plt.imshow(predicted_img, cmap="Blues", alpha=0.1)
+
+        plt.title('Extension model prediction', fontsize=20)
+
