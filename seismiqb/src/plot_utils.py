c672e41805c7e6051850f52e7fd901fa116fecc2
Darima Mylzenova <d.mylzenova@analysiscenter.org>
2019-08-15 12:13:42 +0000

    add docstrings

--- seismiqb/src/plot_utils.py
+++ seismiqb/src/plot_utils.py
@@ -231,3 +231,58 @@ def plot_stratum_predictions(cubes, targets, predictions, n_rows=None):
         axes[i, 0].set_title('Input crop')
         axes[i, 1].set_title('True mask')
         axes[i, 2].set_title('Predicted mask')
+        
+def show_extension_results(batch, val_pipeline, cubes_numbers, ext_result='ext_result',
+                           baseline_result=None, figsize=(25, 10)):
+    """ Demonstrate the results of the Horizon Extension model
+
+    Parameters
+    ----------
+    batch : instance of SeismicCropBatch
+    val_pipeline : instance of Pipeline
+        must contain Pipeline variable that stores model's input tensor, mask and predictions.
+    cube_numbers : array-like of int
+        Index numbers of crops in the batch to show.
+    ext_result : str
+        Name of pipeline variable where extension model results are saved.
+    baseline_result : str, optional
+        Name of pipeline variable where baseline model results are saved.
+    """
+    for cube in cubes_numbers:
+        print(batch.indices[cube][:-10])
+        iline = 0
+
+        truth_img =     val_pipeline.get_variable(ext_result)[0][cube, :, :, iline].T
+        truth_labels =  val_pipeline.get_variable(ext_result)[1][cube, :, :, iline, 0].T
+        predicted_img = val_pipeline.get_variable(ext_result)[2][cube, :, :, iline, 0].T
+        cut_mask =      val_pipeline.get_variable(ext_result)[0][cube, :, :, iline + 2].T
+        if baseline_result:
+            predicted_simple = val_pipeline.get_variable(baseline_result)[2][cube, :, :, iline, 0].T
+
+        fig = plt.figure(figsize=figsize)
+        fig.add_subplot(1, 5, 1)
+        plt.imshow(truth_img, cmap='gray')
+        plt.title('Input tensor', fontsize=20)
+
+        fig.add_subplot(1, 5, 2)
+        plt.imshow(cut_mask, cmap="Blues")
+        plt.imshow(truth_img, cmap="gray", alpha=0.5)
+        plt.title('Input mask', fontsize=20)
+
+        fig.add_subplot(1, 5, 3)
+        plt.imshow(truth_labels, cmap="Blues")
+        plt.imshow(truth_img, cmap="gray", alpha=0.5)
+        plt.title('True mask', fontsize=20)
+    
+        if baseline_result:
+            fig.add_subplot(1, 5, 4)
+            plt.imshow(predicted_simple, cmap="Greens")
+            plt.title('Baseline prediction', fontsize=20)
+            fig.add_subplot(1, 5, 5)
+        else:
+            fig.add_subplot(1, 5, 4)
+        plt.imshow(predicted_img, cmap="Blues")
+        plt.imshow(truth_img, cmap="gray", alpha=0.5)
+        plt.imshow(predicted_img, cmap="Blues", alpha=0.1)
+        plt.title('Extension prediction', fontsize=20)
+        plt.show()
